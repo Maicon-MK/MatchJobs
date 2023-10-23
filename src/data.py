1a7e489cf52e945014348d2564cfe713c8c533eb
mouravitor313 <mouravitor0104@gmail.com>
2023-10-23 00:33:49 -0300

    edit_data_path

--- src/data.py
+++ src/data.py
@@ -4,7 +4,7 @@ from src.interview import Interview
 
 class Data:
     def export(self, results):
-            if not path.exists('C:\\Users\\codgibrazuca\\Documents\\PYTHON\\MatchJobs\\data\\dados.csv'):
+            if not path.exists('data\dados.csv'):
                 return pd.DataFrame(results).to_csv('data/dados.csv', index=False)            
             else:
                 return pd.DataFrame(results).to_csv('data/dados.csv', mode='a', index=False, header=False)
