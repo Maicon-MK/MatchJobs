40146781f5ea6d8a1f8ea92ee1e3e03bd25fae7a
mouravitor313 <mouravitor0104@gmail.com>
2023-10-23 00:23:05 -0300

    questions_improved

--- src/questions.py
+++ src/questions.py
@@ -2,44 +2,17 @@ class Questions:
     def __init__(self):
         self.valid_options = ['A', 'B', 'C', 'D']
         self.intense_options = ['Nenhuma','Pouco','Bastante','Demais']
+        self.questions = ['Pergunta 1','Pergunta 2','Pergunta 3','Pergunta 4']
 
-    def ask_question(self, question_text):
-        answer = input(question_text).capitalize().strip()
+    def get_answer(self):
+        answer = input().capitalize().strip()
         if answer not in self.valid_options:
             raise ValueError('Opção inválida!')
         else:
             return answer
 
-    def question(self):
+    def question(self, question_index):
+        print(self.questions[question_index])
         for option in self.valid_options:
-            for intense in self.intense_options:
-                print (f'{option}. {intense}')
-
-    def first_question(self):
-        return self.ask_question('''Qual é o que?
-    A. Blablabla
-    B. Blablabla
-    C. Claclacla
-    D. Doideira\n''')
-
-    def second_question(self):
-        return self.ask_question('''Qual é o que?
-    A. Blablabla
-    B. Blablabla
-    C. Claclacla
-    D. Doideira\n''')
-
-    def third_question(self):
-        return self.ask_question('''Qual é o que?
-    A. Blablabla
-    B. Blablabla
-    C. Claclacla
-    D. Doideira\n''')
-
-    def fourth_question(self):
-        return self.ask_question('''Qual é o que?
-    A. Blablabla
-    B. Blablabla
-    C. Claclacla
-    D. Doideira\n''')
-
+            print (f'{option}. {self.intense_options[self.valid_options.index(option)]}')
+        return self.get_answer()
